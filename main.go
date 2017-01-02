ba4a3a7e9523f7d27c90816ddd2ac5000360ee84
Gabor Juhasz <juhg24@gmail.com>
2017-01-02 13:58:15 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"html/template"
+	"strings"
+	"encoding/json"
+	"io/ioutil"
+	"errors"
+)
+
+func main() {
+	PORT := "127.0.0.1:8080"
+	log.Print("Running server on "+ PORT)
+	http.HandleFunc("/s/", getMovieFromTitle)
+	http.HandleFunc("/search/", searchHandler)
+	http.HandleFunc("/", frontHandler)
+
+	log.Fatal( http.ListenAndServe(PORT, nil) )
+}
+
+type Series struct {
+	Response, Title, Plot, Year string
+}
+
+func getMovieFromTitle(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/s/"):]
+	s, _ := getData(title)
+	t, _ := template.ParseFiles("templates/series.html")
+	t.Execute(w, s)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("search")
+	s, err := getData(title)
+	if err != nil {
+		http.Redirect(w, r, r.Referer(), http.StatusFound)
+	}
+	title = strings.Replace(s.Title, " ", "+", -1)
+	http.Redirect(w, r, "/s/"+title, http.StatusFound)
+}
+
+func frontHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/home.html")
+	t.Execute(w, nil)
+}
+
+func getData(search string) (Series, error) {
+	// encode the spaces
+	title := strings.Replace(search, " ", "+", -1)
+
+	res, err := http.Get("http://www.omdbapi.com/?type=series&tomatoes=true&t=" + title)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+
+	var s Series
+	err = json.Unmarshal(data, &s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if "False" == s.Response {
+		return s, errors.New("No response found")
+	}
+	return s, nil
+}

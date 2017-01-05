9ba9679a0c8720786cc05eedf3b8b6e8f2369e14
Gabor Juhasz <juhg24@gmail.com>
2017-01-05 10:29:22 +0100

    API: switched to themoviedb.org

--- main.go
+++ main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
+	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/flosch/pongo2"
 	"github.com/gorilla/mux"
-	"strconv"
+	"io/ioutil"
+	"sort"
+	"strings"
 )
 
 var (
-	tmpl = map[string]*pongo2.Template{
+	templates = map[string]*pongo2.Template{
 		"home":   pongo2.Must(pongo2.FromFile("templates/home.html")),
 		"series": pongo2.Must(pongo2.FromFile("templates/series.html")),
+		"search": pongo2.Must(pongo2.FromFile("templates/search.html")),
 	}
 )
+
+const API_KEY = "65b7998bdc0bc00e7fffa6e1d05519dc"
 
 func main() {
 
@@ -29,8 +33,9 @@ func main() {
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/search/", searchHandler)
-	r.HandleFunc("/s/{title}/", seriesHandler)
-	r.HandleFunc("/s/{title}/{season:[0-9]+}/", seriesHandler)
+	r.HandleFunc("/search/{page}/", searchHandler)
+	r.HandleFunc("/s/{id:[0-9]+}/", seriesHandler)
+	//r.HandleFunc("/s/{id:[0-9]+}/{season:[0-9]+}/", seriesHandler)
 	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":"+PORT, nil))
@@ -45,18 +50,71 @@ func getPort() (PORT string) {
 }
 
 type Series struct {
-	Response,
-	Title,
-	Plot,
-	Year,
-	TotalSeasons string
+	Id                 int
+	Vote_count         int
+	Popularity         float32
+	Vote_average       float32
+	In_production      bool
+	Number_of_seasons  int
+	Number_of_episodes int
+	Vote_average_string,
+	Name,
+	Original_name,
+	First_air_date,
+	Overview string
+}
+
+func (s Series) cleanUp() Series {
+
+	// Get the year from the date
+	if len(s.First_air_date) > 3 {
+		s.First_air_date = s.First_air_date[:4]
+	}
+	// Change the decimal places to 2
+	s.Vote_average_string = fmt.Sprintf("%.2f", s.Vote_average)
+
+	return s
+}
+
+type apiResponse struct {
+	Page,
+	Total_results,
+	Total_pages int
+	Results []Series
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates["home"].ExecuteWriter(nil, w)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func seriesHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	title := vars["title"]
+	id := vars["id"]
 	season := vars["season"]
 
+	related, err := getRelated(id)
+	if err != nil {
+		related = []Series{}
+	}
+
+	series, _ := getDetails(id)
+	err = templates["series"].ExecuteWriter(pongo2.Context{
+		"s":      series.cleanUp(),
+		"season": season,
+		//"seasons": seasons,
+		"related": related,
+	}, w)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+TODO: list all the seasons
+func seriesHandler(w http.ResponseWriter, r *http.Request) {
 	series, _ := getData(title)
 	totalSeasons, err := strconv.ParseInt(series.TotalSeasons, 10, 0)
 	if err != nil {
@@ -77,59 +135,52 @@ func seriesHandler(w http.ResponseWriter, r *http.Request) {
 	for i := 0; i < int(totalSeasons); i++ {
 		seasons[i] = i + 1
 	}
-	err = tmpl["series"].ExecuteWriter(pongo2.Context{
-		"s":       series,
-		"season":  season,
-		"seasons": seasons,
-	}, w)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
+}*/
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.FormValue("search")
-	series, err := getData(title)
-	if err != nil {
-		http.Redirect(w, r, r.Referer(), http.StatusFound)
-	}
+func getDetails(id string) (Series, error) {
+	url := "https://api.themoviedb.org/3/tv/" + id + "?api_key=" + API_KEY + "&language=hu-HU"
+	payload := strings.NewReader("{}")
+	req, _ := http.NewRequest("GET", url, payload)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
 
-	title = titleToURL(series.Title)
-	url := "/s/" + title + "/"
-	http.Redirect(w, r, url, http.StatusFound)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	err := tmpl["home"].ExecuteWriter(nil, w)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getData(search string) (Series, error) {
-	title := titleToURL(search)
-	res, err := http.Get("http://www.omdbapi.com/?type=series&t=" + title)
-	if err != nil {
-		log.Fatal(err)
-	}
-	data, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-
-	var s Series
-	err = json.Unmarshal(data, &s)
+	var series Series
+	err := json.Unmarshal(body, &series)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if "False" == s.Response {
-		return s, errors.New("No response found")
-	}
-	return s, nil
+	// TODO: handle error when no series was found
+	//if len(response.Results) == 0 {
+	//	return response, errors.New("No series was found!")
+	//}
+
+	return series, nil
 }
 
-func titleToURL(search string) (title string) {
-	title = search
-	title = strings.Replace(title, " ", "+", -1)
-	title = strings.ToLower(title)
-	return
+func getRelated(id string) ([]Series, error) {
+	url := "https://api.themoviedb.org/3/tv/" + id + "/similar?api_key=" + API_KEY + "&language=hu-HU"
+	payload := strings.NewReader("{}")
+	req, _ := http.NewRequest("GET", url, payload)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var response apiResponse
+	err := json.Unmarshal(body, &response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(response.Results) == 0 {
+		return []Series{}, errors.New("No series was found!")
+	}
+	series := response.Results
+	sort.Sort(ByPopularity(series))
+
+	return series, nil
 }
---
+++ search.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/flosch/pongo2"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	search := r.FormValue("search")
+
+	var response string
+	series, err := getAllResults(search)
+	if err != nil {
+		response = err.Error()
+	}
+
+	err = templates["search"].ExecuteWriter(pongo2.Context{
+		"search":   search,
+		"results":  series,
+		"response": response,
+	}, w)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// The sorting function
+type ByPopularity []Series
+
+func (s ByPopularity) Len() int {
+	return len(s)
+}
+func (s ByPopularity) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s ByPopularity) Less(i, j int) bool {
+	return s[i].Popularity > s[j].Popularity
+}
+
+func getAllResults(search string) (series []Series, err error) {
+	data, err := getSearchResult(search, 1)
+	if err == nil {
+		series = extractResults(series, data)
+	}
+	if data.Total_pages > 1 {
+		for i := 2; i < (data.Total_pages + 1); i++ {
+			data, err := getSearchResult(search, i)
+			if err == nil {
+				series = extractResults(series, data)
+			}
+		}
+	}
+	// Sorting by popularity
+	sort.Sort(ByPopularity(series))
+	return
+}
+
+func extractResults(series []Series, data apiResponse) []Series {
+	for _, s := range data.Results {
+		// Filter the results so that we only show results with more than one vote
+		if s.Vote_count > 1 {
+			series = append(series, s.cleanUp())
+		}
+	}
+	return series
+}
+
+func getSearchResult(search string, page int) (apiResponse, error) {
+	title := titleToURL(search)
+	pageString := strconv.FormatInt(int64(page), 10)
+	url := "https://api.themoviedb.org/3/search/tv?api_key=" + API_KEY + "&page=" + pageString + "&language=hu-HU&query=" + title
+	payload := strings.NewReader("{}")
+	req, _ := http.NewRequest("GET", url, payload)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var response apiResponse
+	err := json.Unmarshal(body, &response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(response.Results) == 0 {
+		return response, errors.New("No series was found!")
+	}
+
+	return response, nil
+}
+
+func titleToURL(search string) string {
+	search = strings.Replace(search, " ", "+", -1)
+	search = strings.ToLower(search)
+	return search
+}
